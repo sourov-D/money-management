ee83f6d83dbca52bdea0f1256e49b823bf946837
Sourov Sharma <sourovsharma31@gmail.com>
2022-09-21 19:56:29 +0600

    clean main

--- main.c
+++ main.c
@@ -6,18 +6,5 @@
 
 int main(int argc, char **argv){
 
-	if(!(strcmp("-cw", *(argv+1)))){
-		WALLET wallet;
-		WALLET *walletp = &wallet;
-		int bal = atoi(*(argv + 3));
-		createWallet(walletp, bal, *(argv + 2));
-	}else if(!(strcmp("-ab", *(argv+1)))){
-		WALLET wallet;
-		WALLET *walletp = &wallet;
-		walletp->balance = atoi(*(argv + 3));
-		strcpy(walletp->walletName, *(argv + 2));
-		addBalance(wallet, amount)
-	}
-
 	return 0;
 }
